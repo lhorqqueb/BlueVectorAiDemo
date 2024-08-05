e5184988604d66e66ccc700f3fdcf0e430bcb449
louishorqque <louis.horqque@gmail.com>
2024-08-05 17:13:59 -0400

    updated test script

--- src/test/scripts/runTests.sh
+++ src/test/scripts/runTests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Step 1: Run Maven tests
-echo "Running Maven tests..."
+echo "============================================== Running Maven tests... =============================================="
 mvn clean test
 
 # Step 2: Generate Allure report
-echo "Generating Allure report..."
+echo "============================================ Generating Allure report... ============================================"
 allure serve allure-results
 
 # Step 3: Clean up project directories
-echo "Cleaning up project directories..."
+echo "========================================= Cleaning up project directories... ========================================"
 rm -rf .idea
 
 # Step 4: Run Maven clean
-echo "Running Maven clean..."
+echo " ============================================= Running Maven clean... ============================================="
 mvn clean
 
-echo "Automated Test Execution completed."
+echo "======================================== Automated Test Execution completed ========================================"
